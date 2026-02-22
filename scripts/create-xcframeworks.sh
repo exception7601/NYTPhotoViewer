8165f3246a88755b898d45904f93bd0a7e7d1fa6
and <77030548+exception7601@users.noreply.github.com>
2026-02-22 08:59:36 -0300

    Fix git tag command syntax in create-xcframeworks.sh

--- scripts/create-xcframeworks.sh
+++ scripts/create-xcframeworks.sh
@@ -43,7 +43,7 @@ DOWNLOAD_URL="https://github.com/${REPO}/releases/download/${NEW_VERSION}/${NAME
 
 git add version
 git commit -m "new Version ${NEW_VERSION}"
-git tag -s -a "${NEW_VERSION}" -m "v${NEW_VERSION}"
+git tag -a "${NEW_VERSION}" -m "v${NEW_VERSION}"
 git push origin HEAD --tags
 gh release create "${NEW_VERSION}" "${NAME}" --notes "checksum \`${SUM}\`"
 
